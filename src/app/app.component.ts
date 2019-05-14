4a1c4310bcbb8b649e9517e22f65629f58eb1d02
Martin Matovu <matovu.synergy@gmail.com>
2019-05-14 16:05:45 +0300

    Import "Product", add a property "products" of type "Product[ ]" & instantiate "products".

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ang-inventory-app';
+
+  products: Product[];
+
+  constructor () {
+    this.products = [];
+  }
 }
