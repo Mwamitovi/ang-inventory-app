e6d4d06a1b936a91978d87ec66c67515cc06684a
Martin Matovu <matovu.synergy@gmail.com>
2019-05-14 16:11:29 +0300

    Created 3 products in the "constructor"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,25 @@ export class AppComponent {
   products: Product[];
 
   constructor () {
-    this.products = [];
+    this.products = [
+      new Product(
+        'MYSHOES',
+        'Black Running Shoes',
+        'assets/images/products/black-shoes.jpg',
+        ['Men', 'Shoes', 'Running Shoes'],
+        134900),
+      new Product(
+        'NEATOJACKET',
+        'Blue Jacket',
+        'assets/images/products/blue-jacket.jpg',
+        ['Women', 'Apparel', 'Jackets & Vests'],
+        250000),
+      new Product(
+        'AFRICAHAT',
+        'A Nice African Hat',
+        'assets/images/products/black-hat.jpg',
+        ['Men', 'Accessories', 'African', 'Hats'],
+        155000)
+    ];
   }
 }
