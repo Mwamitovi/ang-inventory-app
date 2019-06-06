cf567ca09816242e390cb1b3d5e52964563c1ea8
Martin Matovu <matovu.synergy@gmail.com>
2019-06-06 18:03:57 +0300

    Created in-line markup for template, and declared @Input() within the PriceDisplayComponent() class

--- src/app/price-display/price-display.component.ts
+++ src/app/price-display/price-display.component.ts
@@ -1,11 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
+/**
+ * @PriceDisplay: A component to show the price of a Product
+ */
 @Component({
   selector: 'app-price-display',
-  templateUrl: './price-display.component.html',
+  // templateUrl: './price-display.component.html',
+  template: `
+    <div class="price-display">\${{ price }}</div>
+  `,
   styleUrls: ['./price-display.component.css']
 })
 export class PriceDisplayComponent implements OnInit {
+
+  @Input() price: number;
 
   constructor() { }
 
