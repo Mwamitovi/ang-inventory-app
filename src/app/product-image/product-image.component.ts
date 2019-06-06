76611c647ea33ebc1572cd83be43258535f69b76
Martin Matovu <matovu.synergy@gmail.com>
2019-06-06 17:26:18 +0300

    Imported "Product", created in-line template, and declared @Input() and @HostBinding() within ProductImageComponent() class

--- src/app/product-image/product-image.component.ts
+++ src/app/product-image/product-image.component.ts
@@ -1,11 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, HostBinding } from '@angular/core';
+import { Product } from '../product.model';
+
+/**
+ * @ProductImage: A component to show a single Product's image
+ */
 
 @Component({
   selector: 'app-product-image',
-  templateUrl: './product-image.component.html',
+  // templateUrl: './product-image.component.html',
+  template: `
+    <img class="product-image" [src]="product.imageUrl"
+  `,
   styleUrls: ['./product-image.component.css']
 })
 export class ProductImageComponent implements OnInit {
+
+  @Input() product: Product;
+
+  @HostBinding('attr.class') cssClass = 'ui small image';
 
   constructor() { }
 
